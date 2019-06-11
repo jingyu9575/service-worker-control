9cefc4fc6cce8e9354a84a10c46431013be16dcd
jingyu9575 <qw2g64@outlook.com>
2019-06-12 00:17:53 +0800

    Apply theme color to pageAction. Close #3

--- background.js
+++ background.js
@@ -5,6 +5,51 @@ browser.runtime.onMessage.addListener((message, { tab }) => {
 	}
 })
 
+const iconCache = {}
+
+async function getIcon(path) {
+	let color
+	try {
+		// Not needing `theme` permission [Firefox 62]
+		const { colors } = await browser.theme.getCurrent()
+		if (colors) {
+			const c = colors.icons || colors.toolbar_text || colors.bookmark_text
+			color = Array.isArray(c) ?
+				`${'rgba'.slice(0, c.length)}(${c.join(',')})` : c
+		}
+	} catch { }
+	if (!color) return { path }
+
+	const key = `${path}__${color}`
+	if (iconCache[key]) return iconCache[key]
+
+	const xml = await (await fetch(path)).text()
+	const node = new DOMParser().parseFromString(xml, 'image/svg+xml').documentElement
+
+	// toolbar/menupanel icon size is always 16 * devicePixelRatio [Firefox 68.0b5]
+	const SIZE_PX = 16
+	const size = Math.ceil(SIZE_PX * devicePixelRatio)
+	const img = new Image(size, size)
+	node.style.fill = color
+	node.setAttribute('width', '' + size)
+	node.setAttribute('height', '' + size)
+
+	await new Promise(resolve => {
+		img.addEventListener('load', resolve)
+		img.src = "data:image/svg+xml," + encodeURIComponent(node.outerHTML)
+	})
+	img.width = size
+	img.height = size
+
+	const canvas = document.createElement('canvas')
+	canvas.width = size
+	canvas.height = size
+	const context = canvas.getContext('2d')
+	context.imageSmoothingEnabled = false
+	context.drawImage(img, 0, 0)
+	return { imageData: { [SIZE_PX]: context.getImageData(0, 0, size, size) } }
+}
+
 async function updateTab(tabId) {
 	const all = await getAllRegistrations(tabId)
 	const pendings = all.filter(v => v.pending)
@@ -15,12 +60,17 @@ async function updateTab(tabId) {
 				browser.i18n.getMessage('nRegisteredNPending',
 					[all.length - pendings.length, pendings.length])
 	})
-	browser.pageAction.setIcon({
-		tabId, path: pendings.length ? "icons/icon-pending.svg" : null
-	})
+	browser.pageAction.setIcon(Object.assign({ tabId },
+		await getIcon(`icons/icon${pendings.length ? "-pending" : ""}.svg`)))
 	browser.pageAction[all.length ? 'show' : 'hide'](tabId)
 }
 
-browser.tabs.query({ windowType: "normal" }).then(tabs => {
-	for (const { id } of tabs) updateTab(id).catch(() => { })
-})
+function updateAllTabs() {
+	browser.tabs.query({ windowType: "normal" }).then(tabs => {
+		for (const { id } of tabs) updateTab(id).catch(() => { })
+	})
+}
+updateAllTabs()
+
+// Not needing `theme` permission [Firefox 62]
+try { browser.theme.onUpdated.addListener(updateAllTabs) } catch { }
